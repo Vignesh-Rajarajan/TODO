628c68cd26e9e39613a366719452602dc4bf08c3
Vignesh Rajarajan <vicky209180@gmail.com>
2015-12-13 18:29:55 +0530

    added sqlite to apis

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+module.exports=function(sequelize,dataTypes){
+return sequelize.define('todo',{
+	description:{
+		type:dataTypes.STRING,
+		notNULL:false,
+		validate:{
+			len:[1,250]
+		}
+	},
+	completed:{
+		type:dataTypes.BOOLEAN,
+		defaultValues:false,
+		allowNull:false
+	}
+});
+};
--- server.js
+++ server.js
@@ -1,41 +1,57 @@
-var express=require('express');
+var express = require('express');
 var app = express();
-var port =process.env.PORT || 3000;
-var bodyparser=require('body-parser');
-var todos=[];
-var todoNextId=1;
-var _= require('underscore');
-app.get('/',function(req,res){
+var port = process.env.PORT || 3000;
+var bodyparser = require('body-parser');
+var todos = [];
+var todoNextId = 1;
+var _ = require('underscore');
+var db = require('./db.js');
+
+
+
+app.get('/', function(req, res) {
 	res.send('Welcome to TO-DO api');
 });
 app.use(bodyparser.json());
 
-app.get('/listall',function(req,res){
-	var copiedVal=todos;
-	var searchStr=req.query;
+
+app.get('/listall', function(req, res) {
+
+	var copiedVal = db.todo.findAll();
+	var searchStr = req.query;
+	console.log(typeof searchStr);
+	//searchStr=searchStr.trim();
 	console.log(searchStr);
-    if(searchStr.hasOwnProperty('completed') && searchStr.completed==='true'){
-    	console.log(searchStr.completed);
-    	copiedVal=_.where(copiedVal,{'completed':true});
+	if(searchStr.length>0){
+	console.log(searchStr);
+	if (searchStr.hasOwnProperty('completed') && searchStr.completed === 'true') {
+		console.log(searchStr.completed);
+		copiedVal = _.where(copiedVal, {
+			'completed': true
+		});
 
-console.log('sddddd'+copiedVal);
-    }else if(searchStr.hasOwnProperty('completed') && searchStr.completed==='false'){
-    	console.log('eeeeeeeeemessage');
-copiedVal=_.where(copiedVal,{'completed':false});
-    }else if(searchStr.completed.trim().length<=0){
-    	res.status(400).send();
-    }else{
+		console.log('sddddd' + copiedVal);
+	} else if (searchStr.hasOwnProperty('completed') && searchStr.completed === 'false') {
+		console.log('eeeeeeeeemessage');
+		copiedVal = _.where(copiedVal, {
+			'completed': false
+		});
+	} /*else if (searchStr.completed.toString().trim().length <= 0) {
+		res.status(400).send();
+	} */else {
 
-    }
-
-console.log(JSON.stringify(copiedVal));
+	}
+}
+	//console.log(JSON.stringify(copiedVal));
 	res.json(copiedVal);
 });
 
-app.get('/listall/:id',function(req,res){
-	var resp='';
-	var id=parseInt(req.params.id)
-	var matchedTodo=_.findWhere(todos,{id:id})
+app.get('/listall/:id', function(req, res) {
+	var resp = '';
+	var id = parseInt(req.params.id)
+	var matchedTodo = _.findWhere(todos, {
+		id: id
+	})
 	console.log(matchedTodo);
 	/*todos.forEach(function(todo){
 		console.log(todo);
@@ -46,74 +62,88 @@ app.get('/listall/:id',function(req,res){
 			resp=todo;
 		}
 	})*/
-if(matchedTodo){res.json(matchedTodo);}
-else{
-	res.status(404).send();
-}
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 
 });
 
 
-app.post('/todos',function(req,res){
-	var newTodobef=req.body;
-	var newTodo=_.pick(newTodobef,'description','completed')
-if(!_.isBoolean(newTodo.completed)||!_.isString(newTodo.description))
+app.post('/todos', function(req, res) {
+	var newTodobef = req.body;
+	var newTodo = _.pick(newTodobef, 'description', 'completed')
+	if (!_.isBoolean(newTodo.completed) || !_.isString(newTodo.description))
 
 
-{
-	res.status(400).send();
-}
 
-	newTodo.id=todoNextId;
-	todos.push(newTodo);
-	console.log(newTodo);
+	{
+		res.status(400).send();
+	}
+
+	newTodo.id = todoNextId++;
+	
+	db.todo.create(newTodo).then(function(todo){
+		res.json(todo.toJSON());
+	},function(e){
+		res.status(400).json(e);
+	});
+	
+
+});
+
+
+app.delete('/listall/delete/:id', function(req, res) {
+	var id = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {
+		id: id
+	})
+	if (matchedTodo) {
+		todos = _.without(todos, matchedTodo);
+	} else res.status(404).json({
+		"error": "nothing found on that id"
+	});
+
+
 	res.json(todos);
-	todoNextId++;
 
 });
 
 
-app.delete('/listall/delete/:id',function(req,res){
-         var id= parseInt(req.params.id);
-         var matchedTodo=_.findWhere(todos,{id:id})
-         if(matchedTodo){todos=_.without(todos,matchedTodo);}
-         else res.status(404).json({"error":"nothing found on that id"});
-         
+app.put('/listall/put/:id', function(req, res) {
+	var beforUpdate = _.pick(req.body, 'description', 'completed');
 
-res.json(todos);
+	var updatedTodo = {};
 
-});
-
-
-app.put('/listall/put/:id',function(req,res){
-var beforUpdate=_.pick(req.body,'description','completed');
-
-var updatedTodo={};
-
-if(beforUpdate.hasOwnProperty('completed') && _.isBoolean(beforUpdate.completed) ){
-updatedTodo.completed=beforUpdate.completed;
-}else if(beforUpdate.hasOwnProperty('completed')){
-res.status(400).send();
-}
-else{}
-if(beforUpdate.hasOwnProperty('description') && _.isString(beforUpdate.description) && beforUpdate.description.trim().length>0 ){
-updatedTodo.description=beforUpdate.description;
-}else if(beforUpdate.hasOwnProperty('description')){
-res.status(400).send();
-}
-else{}
-var id=parseInt(req.params.id)
-	var matchedTodo=_.findWhere(todos,{id:id})
-	if(matchedTodo){
-		updatedTodo=_.extend(matchedTodo,updatedTodo);
+	if (beforUpdate.hasOwnProperty('completed') && _.isBoolean(beforUpdate.completed)) {
+		updatedTodo.completed = beforUpdate.completed;
+	} else if (beforUpdate.hasOwnProperty('completed')) {
+		res.status(400).send();
+	} else {}
+	if (beforUpdate.hasOwnProperty('description') && _.isString(beforUpdate.description) && beforUpdate.description.trim().length > 0) {
+		updatedTodo.description = beforUpdate.description;
+	} else if (beforUpdate.hasOwnProperty('description')) {
+		res.status(400).send();
+	} else {}
+	var id = parseInt(req.params.id)
+	var matchedTodo = _.findWhere(todos, {
+		id: id
+	})
+	if (matchedTodo) {
+		updatedTodo = _.extend(matchedTodo, updatedTodo);
 		todos.push(updatedTodo);
 
-	}else{
-		res.status(400).json({"error":"requested component no more"});
+	} else {
+		res.status(400).json({
+			"error": "requested component no more"
+		});
 	}
 	res.json(todos);
 });
 
-app.listen(port,function(){
-	console.log('listening on port'+port);
+db.sequelize.sync().then(function() {
+	app.listen(port, function() {
+		console.log('listening on port' + port);
+	});
 });
