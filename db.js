eb01377d76e826697e584329098d708301789fc3
Vignesh Rajarajan <vicky209180@gmail.com>
2015-12-14 22:31:06 +0530

    added DB for production

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
-
-
 var Sequelize=require('sequelize');
-var sequelize=new Sequelize(undefined,undefined,undefined,{
+var sequelize;
+var env= process.env.NODE_ENV||'development';
+if(env==='production'){
+	sequelize=new Sequelize(process.env.DATABASE_URL,{
+	'dialect':'postgresql'
+});
+}
+else   if(env==='development'){
+	sequelize=new Sequelize(undefined,undefined,undefined,{
 	'dialect':'sqlite',
 	'storage':__dirname+'/data/dev-todo.sqlite'
 });
+}
 
 var db={};
 db.todo= sequelize.import(__dirname+'/models/todo.js');
 db.sequelize=sequelize;
 db.Sequelize=Sequelize;
-
 module.exports=db;
